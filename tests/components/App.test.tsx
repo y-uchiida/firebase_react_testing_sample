10ba3568c6c3a04c6cf5761ef55ab615572bf1ca
yugo uchiida <y.uchiida@nirneege.com>
2023-01-01 16:55:19 +0900

    チャット画面用コンポーネントとテストファイルの作成

---
+++ tests/components/App.test.tsx
@@ -0,0 +1,18 @@
+import {
+	render,
+	cleanup,
+	screen
+} from '@testing-library/react';
+import { App } from '@/components/App';
+
+describe('App', () => {
+	afterEach(() => {
+		cleanup();
+	})
+
+	it('タイトルテキストが表示される', async () => {
+		render(<App />);
+
+		expect(screen.getByText('Sample Chat App')).toBeInTheDocument();
+	});
+});
