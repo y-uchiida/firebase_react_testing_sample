be40d1025851e033f2f89f9fc29ed3599db9c545
yugo uchiida <y.uchiida@nirneege.com>
2023-01-06 21:59:46 +0900

    ServiceWorker の設定ファイルを追加

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+import firebaseConfig from "./firebaseConfig.json";
+
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+firebase.initializeApp({
+  apyKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+});
+
+const messaging = firebase.messaging();
